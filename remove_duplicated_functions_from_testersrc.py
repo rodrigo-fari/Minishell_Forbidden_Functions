e7a660e2814ba65c98de30a566788c03455ebbfc
rodrigo-fari <rodrigofal44@gmail.com>
2025-05-20 23:01:38 -0400

    testersrc is now minishell based. Search old commits to find full list.

--- remove_duplicated_functions_from_testersrc.py
+++
@@ -1,5 +0,0 @@
-with open("testersrc", "r") as f:
-    linhas = f.readlines()
-linhas_unicas = list(dict.fromkeys(linhas))  # Mantém a ordem original
-with open("lista_sem_duplicatas.txt", "w") as f:
-    f.writelines(linhas_unicas)
